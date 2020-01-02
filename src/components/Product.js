7a6116ff5e6da295722402e5d7d4b7d33e0c921a
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-02 19:42:27 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,24 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+
+}
+let weight_range = [];
+for (let i = 80; i <= 300; i++) {
+  weight_range.push(i);
+}
+
+// React.PropTypes.oneOf(weight_range).isRequired
+Product.propTypes = {
+    name: propTypes.string.isRequired,
+    producer: propTypes.string,
+    hasWatermark: propTypes.bool,
+    color: propTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+    weight: propTypes.onOf(weight_range)
+};
+
+Product.defaultProps = {
+  hasWatermark: false
+};
